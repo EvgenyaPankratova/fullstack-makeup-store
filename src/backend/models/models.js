7c7291ee2ae4601ad73be243b8d479a853898ee7
Evgenya Pankratova <janice0free@gmail.com>
2023-12-08 00:30:14 +0500

    deleted env

---
+++ src/backend/models/models.js
@@ -0,0 +1,9 @@
+const sequelize = require('../db');
+const {DataTypes} = require('sequelize'); //с помощью DataTypes описываем типы полей
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: "USER"}
+})
